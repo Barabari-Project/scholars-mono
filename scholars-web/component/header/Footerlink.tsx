669b76c88a379156a32f51de3aeeffd8b0a661a0
Sohamsinh <dabhisohamsinh1044@gmail.com>
2023-05-14 16:35:27 +0530

    resolved merge conflict js to tsx with New About Page

--- scholars-web/component/header/Footerlink.tsx
+++ scholars-web/component/header/Footerlink.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import style from "../../styles/footer.module.css";
+import style from '../../styles/footer.module.css';
 
 
 function Footerlink() {
   return (
     <>
     <div>
-    <footer class={style.footerdistributed}>
+    <footer className={style.footerdistributed}>
     
-    <div class={style.footerleft}>
+    <div className={style.footerleft}>
         <h3>Scholarship<span>_Villa</span></h3>
     
-        <p class={style.footerlinks}>
+        <p className={style.footerlinks}>
             <a href="#">Home</a>
             |
             <a href="#">About</a>
@@ -21,27 +21,27 @@ function Footerlink() {
             <a href="#">Blog</a>
         </p>
     
-        <p class={style.footercompanyname}>Copyright © 2023 <strong>thebarabariproject</strong> All rights reserved</p>
+        <p className={style.footercompanyname}>Copyright © 2023 <strong>thebarabariproject</strong> All rights reserved</p>
     </div>
     
-    <div class={style.footercenter}>
+    <div className={style.footercenter}>
         <div>
-            <i class="fa fa-map-marker"></i>
+            <i className="fa fa-map-marker"></i>
             <p><span>Telangana</span>
                 Hydrabad</p>
         </div>
     
         <div>
-            <i class="fa fa-phone"></i>
+            <i className="fa fa-phone"></i>
             <p>+91 74**9**258</p>
         </div>
         <div>
-            <i class="fa fa-envelope"></i>
+            <i className="fa fa-envelope"></i>
             <p><a href="mailto:thebarabariproject@gmail.com">thebarabariproject@gmail.com</a></p>
         </div>
     </div>
-    <div class={style.footerright}>
-        <p class={style.footercompanyabout}>
+    <div className={style.footerright}>
+        <p className={style.footercompanyabout}>
             <span>About the Organization</span>
             <strong>The barabari project</strong> is a NGO where more than 22 candidates are IN TRAINING 
             Students Here trained by Mentor Of Top IT Companies
@@ -49,12 +49,12 @@ function Footerlink() {
             from "4 Different STATES"
         </p>
         
-        <div class={style.footericons}>
-           <div class={style.icons1}> <a href="#"><i class="fa-brands fa-facebook"></i></a></div>
-           <div class={style.icons2}> <a href="https://www.instagram.com/thebarabariproject/"><i class="fa-brands fa-instagram" ></i></a></div>
-           <div class={style.icons3}> <a href="https://www.linkedin.com/company/the-barabari-project/"><i class="fa-brands fa-linkedin"></i></a></div>
-           <div class={style.icons5}> <a href="#"><i class="fa-brands fa-youtube"></i></a></div>
-           <div class={style.icons4}> <a href="https://twitter.com/BarabariProject"><i class="fa-brands fa-twitter"></i></a></div>
+        <div className={style.footericons}>
+           <div className={style.icons1}> <a href="#"><i className="fa-brands fa-facebook"></i></a></div>
+           <div className={style.icons2}> <a href="https://www.instagram.com/thebarabariproject/"><i className="fa-brands fa-instagram" ></i></a></div>
+           <div className={style.icons3}> <a href="https://www.linkedin.com/company/the-barabari-project/"><i className="fa-brands fa-linkedin"></i></a></div>
+           <div className={style.icons5}> <a href="#"><i className="fa-brands fa-youtube"></i></a></div>
+           <div className={style.icons4}> <a href="https://twitter.com/BarabariProject"><i className="fa-brands fa-twitter"></i></a></div>
         </div>
         
     </div>
